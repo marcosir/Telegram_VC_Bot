f8c7db6237a06fd58bd966f5583c092a36459f53
marcosir <79353892+marcosir@users.noreply.github.com>
2021-04-12 11:47:30 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
-API_ID = 14371
-API_HASH = "e46b6c854d2bf58a0"
-SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
-SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
+API_ID = 1686243
+API_HASH = "e6ff820963be298cfef580c81e5ee281"
+SUDO_CHAT_ID = -1001178564235  # Chat where the bot will play the music.
+SUDOERS = [1569115700] # Users which have special control over the bot.
 
 # don't make changes below this line
 ARQ_API = "http://35.240.133.234:8000"
